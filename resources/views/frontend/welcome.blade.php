3540003c346f7d7548c40b46755736143aafe420
Vincent Wambugu <vinsentwambugu@gmail.com>
2024-10-22 23:48:14 +0300

    Final Corrections

--- resources/views/frontend/welcome.blade.php
+++ resources/views/frontend/welcome.blade.php
@@ -3,7 +3,7 @@
 
 
 <head>
-    <title>NECO &mdash; Newness Counselling</title>
+    <title>NECO &mdash; Newness Counselling Organisation</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900" rel="stylesheet">
